6fd4d4f53499f220885bb8c05211560baad45780
Mark Mavromatis <markm@palamida.com>
2016-10-20 11:11:56 -0700

    Added webpage generator: 'fresh_tomatoes'

--- ProgrammingFoundationsWithPython/Lesson5/entertainment_center.py
+++ ProgrammingFoundationsWithPython/Lesson5/entertainment_center.py
@@ -1,4 +1,6 @@
 import media
+import fresh_tomatoes
+
 
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
@@ -11,16 +13,42 @@ avatar = media.Movie("Avatar",
                      "http://www.youtube.com/watch?v=-9ceBgWV8io")
 print(avatar.storyline)
 
-# avatar.show_trailer()
-
-when_harry_met_sally = media.Movie("When Harry Met Sally",
-                                   "Boy meets girl several times, then falls in love",
-                                   "https://en.wikipedia.org/wiki/File:WhenHarryMetSallyPoster.jpg",
-                                   "https://www.youtube.com/watch?v=vmSpCLefjnw")
-
-# when_harry_met_sally.show_trailer()
 school_of_rock = media.Movie("School of Rock",
                              "Using rock music to learn",
                              "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
                              "https://www.youtube.com/watch?v=3PsUJFEBC74")
 
+ratatouille = media.Movie("Ratatouille",
+                          "A rat is a chef in Paris",
+                          "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_paris = media.Movie("Midnight in Paris",
+                                "Going back in time to meet authors",
+                                "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                                "https://www.youtube.com/watch?v=FAfR8omt-CY")
+
+hunger_games = media.Movie("Hunger Games",
+                           "A really real reality show",
+                           "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+                           "https://www.youtube.com/watch?v=PbA63a7H0bo")
+
+when_harry_met_sally = media.Movie("When Harry Met Sally",
+                                   "Boy meets girl several times, then falls in love",
+                                   "https://upload.wikimedia.org/wikipedia/en/1/1c/WhenHarryMetSallyPoster.jpg",
+                                   "https://www.youtube.com/watch?v=vmSpCLefjnw")
+
+before_sunrise = media.Movie("Before Sunrise",
+                                   "Boy meets girl on train...",
+                                   "https://upload.wikimedia.org/wikipedia/en/d/da/Before_Sunrise_poster.jpg",
+                                   "https://www.youtube.com/watch?v=9v6X-Dytlko")
+
+before_sunset = media.Movie("Before Sunset",
+                                   "Boy reunites with girl after 10-years...",
+                                   "https://upload.wikimedia.org/wikipedia/en/d/d1/Before_Sunset_poster.jpg",
+                                   "https://www.youtube.com/watch?v=oI3UuneLcyU")
+
+
+movies = [before_sunrise, before_sunset, toy_story, avatar, when_harry_met_sally, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+
+fresh_tomatoes.open_movies_page(movies)
